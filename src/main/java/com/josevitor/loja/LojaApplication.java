cef98269631a24f0c0b02bf5769c20b843431a93
José Vitor <jose.freitas16@fatec.sp.gov.br>
2020-06-08 18:51:01 -0300

    Criação de operação de instanciação

--- src/main/java/com/josevitor/loja/LojaApplication.java
+++ src/main/java/com/josevitor/loja/LojaApplication.java
@@ -1,13 +1,33 @@
 package com.josevitor.loja;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class LojaApplication {
+import com.josevitor.loja.domain.Categoria;
+import com.josevitor.loja.repositories.CategoriaRepository;
 
+@SpringBootApplication
+public class LojaApplication implements CommandLineRunner{
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+		
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+		
 	}
 
 }
