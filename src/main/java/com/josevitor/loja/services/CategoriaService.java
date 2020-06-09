f9e13d22cf81602b3f9d6a10cc96fbb8b25734f4
José Vitor <jose.freitas16@fatec.sp.gov.br>
2020-06-09 17:56:24 -0300

    Cidade e Estado

--- src/main/java/com/josevitor/loja/services/CategoriaService.java
+++ src/main/java/com/josevitor/loja/services/CategoriaService.java
@@ -7,16 +7,17 @@ import org.springframework.stereotype.Service;
 
 import com.josevitor.loja.domain.Categoria;
 import com.josevitor.loja.repositories.CategoriaRepository;
+import com.josevitor.loja.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
 
 	public Categoria find(Integer id) {
-		 Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
-	} 
-
+		Optional<Categoria> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
+	}
 }
---
+++ src/main/java/com/josevitor/loja/services/exceptions/ObjectNotFoundException.java
@@ -0,0 +1,14 @@
+package com.josevitor.loja.services.exceptions;
+
+public class ObjectNotFoundException extends RuntimeException {
+
+	private static final long serialVersionUID = 1L;
+	
+	public ObjectNotFoundException(String msg) {
+		super(msg);
+	}
+
+	public ObjectNotFoundException(String msg, Throwable cause) {
+		super(msg, cause);
+	}
+}
