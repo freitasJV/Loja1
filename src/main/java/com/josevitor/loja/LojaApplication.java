2edbfc9ad3fa72e7cb830d68700b144ad518a4ab
José Vitor <jose.freitas16@fatec.sp.gov.br>
2020-06-09 21:10:17 -0300

    Corrigindo referência cíclica e nome de métodos getters e setters

--- src/main/java/com/josevitor/loja/LojaApplication.java
+++ src/main/java/com/josevitor/loja/LojaApplication.java
@@ -45,9 +45,9 @@ public class LojaApplication implements CommandLineRunner {
 		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 
-		p1.getCategoria().addAll(Arrays.asList(cat1));
-		p2.getCategoria().addAll(Arrays.asList(cat1, cat2));
-		p3.getCategoria().addAll(Arrays.asList(cat1));
+		p1.getCategorias().addAll(Arrays.asList(cat1));
+		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
+		p3.getCategorias().addAll(Arrays.asList(cat1));
 
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1, p2, p3));
--- src/main/java/com/josevitor/loja/domain/Produto.java
+++ src/main/java/com/josevitor/loja/domain/Produto.java
@@ -12,7 +12,7 @@ import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToMany;
 
-import com.fasterxml.jackson.annotation.JsonBackReference;
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 @Entity
 public class Produto implements Serializable {
@@ -24,7 +24,7 @@ public class Produto implements Serializable {
 	private String nome;
 	private Double preco;
  
-	@JsonBackReference
+	@JsonIgnore
 	@ManyToMany
 	@JoinTable(name = "PRODUTO_CATEGORIA", joinColumns = @JoinColumn(name = "produto_id"), inverseJoinColumns = @JoinColumn(name = "categoria_id"))
 	private List<Categoria> categorias = new ArrayList<>();
@@ -63,11 +63,11 @@ public class Produto implements Serializable {
 		this.preco = preco;
 	}
 
-	public List<Categoria> getCategoria() {
+	public List<Categoria> getCategorias() {
 		return categorias;
 	}
 
-	public void setCategoria(List<Categoria> categoria) {
+	public void setCategorias(List<Categoria> categoria) {
 		this.categorias = categoria;
 	}
 
