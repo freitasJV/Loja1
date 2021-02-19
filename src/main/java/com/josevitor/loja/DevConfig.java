158b1143423d2a5149aad0fe36091af0b1a4b6a7
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-02-19 10:21:52 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/josevitor/loja/DevConfig.java
@@ -0,0 +1,33 @@
+package com.josevitor.loja;
+
+import java.text.ParseException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.josevitor.loja.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+	
+	@Value("${spring.jpa.hibernate.ddl-auto}")
+	private String strategy;
+	
+	@Bean
+	public boolean instantiateDatabase() throws ParseException {
+		if(!strategy.equals("create")) {
+			return false;
+		}
+		
+		
+		dbService.instantianteTesteDatabase();		
+		return true;
+	}
+}
