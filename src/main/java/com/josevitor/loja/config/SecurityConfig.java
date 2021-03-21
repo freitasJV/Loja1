ac1ce239e4ec571fe667ab6a10c6952f69ccf38a
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-03-21 20:29:38 -0300

    Implementando autorização

--- src/main/java/com/josevitor/loja/config/SecurityConfig.java
+++ src/main/java/com/josevitor/loja/config/SecurityConfig.java
@@ -19,6 +19,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 import com.josevitor.loja.security.JWTAuthenticationFilter;
+import com.josevitor.loja.security.JWTAuthorizationFilter;
 import com.josevitor.loja.security.JWTUtil;
 
 @Configuration
@@ -58,6 +59,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 			.anyRequest().authenticated();
 		
 		http.addFilter(new JWTAuthenticationFilter(authenticationManager(), jwtUtil));
+		http.addFilter(new JWTAuthorizationFilter(authenticationManager(), jwtUtil, userDetailsService));
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
 	}
 	
