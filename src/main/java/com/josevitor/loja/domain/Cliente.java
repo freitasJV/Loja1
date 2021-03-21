cb45255b178dc0bb2dbf73b6edf68bf9bf49f950
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-03-21 16:20:01 -0300

    Adicionando senha ao Cliente

--- src/main/java/com/josevitor/loja/domain/Cliente.java
+++ src/main/java/com/josevitor/loja/domain/Cliente.java
@@ -32,6 +32,9 @@ public class Cliente implements Serializable {
 	private String email;
 	private String cpfCnpj;
 	private Integer tipo;
+	
+	@JsonIgnore
+	private String senha;
 
 	@OneToMany(mappedBy = "cliente", cascade = CascadeType.ALL)
 	private List<Endereco> enderecos = new ArrayList<>();
@@ -48,13 +51,14 @@ public class Cliente implements Serializable {
 
 	}
 
-	public Cliente(Integer id, String nome, String email, String cpfCnpj, TipoCliente tipo) {
+	public Cliente(Integer id, String nome, String email, String cpfCnpj, TipoCliente tipo, String senha) {
 		super();
 		this.id = id;
 		this.nome = nome;
 		this.email = email;
 		this.cpfCnpj = cpfCnpj;
 		this.tipo = tipo == null ? null : tipo.getCod();
+		this.senha = senha;
 	}
 
 	public Integer getId() {
@@ -96,10 +100,18 @@ public class Cliente implements Serializable {
 	public void setTipo(TipoCliente tipo) {
 		this.tipo = tipo.getCod();
 	}
+	
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
 
 	public List<Endereco> getEnderecos() {
 		return enderecos;
-	}
+	}	
 
 	public void setEnderecos(List<Endereco> enderecos) {
 		this.enderecos = enderecos;
