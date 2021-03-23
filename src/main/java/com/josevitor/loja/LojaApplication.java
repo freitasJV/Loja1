6921fa7f8dedc64a6247ddda23a2fa3eaab3259f
freitasJV <jose.freitas16@fatec.sp.gov.br>
2021-03-22 23:29:23 -0300

    Salvando primeiro arquivo no S3

--- src/main/java/com/josevitor/loja/LojaApplication.java
+++ src/main/java/com/josevitor/loja/LojaApplication.java
@@ -1,18 +1,25 @@
 package com.josevitor.loja;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.josevitor.loja.services.S3Service;
+
 @SpringBootApplication
 public class LojaApplication implements CommandLineRunner {
 
+	@Autowired
+	private S3Service s3Service;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(LojaApplication.class, args);
 	}
 
 	@Override
-	public void run(String... args) throws Exception {				
+	public void run(String... args) throws Exception {		
+		s3Service.uploadFile("C:\\temp\\TesteAWS.jpg");
 	}
 
 }
